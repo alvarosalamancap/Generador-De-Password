19a3af071811400a594798f87a26ef65e55cedc4
A L V A R O <157981373+alvarosalamancap@users.noreply.github.com>
2024-01-28 15:06:36 -0300

    generadorContraseñasSimple.py
    Se le agrego la opcion para salir con la letra "S"

--- generadorContraseñasSimple.py
+++ generadorContraseñasSimple.py
@@ -1,12 +1,24 @@
 import string
 import random
-print("\t---------------------------------------------------------")
-print("\tBIENVENIDOS AL GENERADOR DE CONTRASEÑAS SEGURAS DE ALVARO")
-print("\t---------------------------------------------------------")
-longitud = int(input("\tIngresar Tamaño Que Quieres Que Tenga Tu Contraseña: "))
 
-caracteres = string.ascii_letters + string.digits + string.punctuation
+while True:
+    print("\t---------------------------------------------------------")
+    print("\tBIENVENIDOS AL GENERADOR DE CONTRASEÑAS SEGURAS DE ALVARO")
+    print("\t---------------------------------------------------------")
 
-contrasena = "".join(random.choice(caracteres) for i in range(longitud))
 
-print(f"\tLa contraseña que se generó es: {contrasena} ")
+    longitud = int(input("\tIngresar Tamaño Que Quieres Que Tenga Tu Contraseña: "))
+
+    if longitud <= 0:
+        print("\tLa longitud de la contraseña debe ser mayor que cero. Inténtalo de nuevo.")
+        continue
+
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+
+    contrasena = "".join(random.choice(caracteres) for i in range(longitud))
+
+    print(f"\tLa contraseña que se generó es: {contrasena} ")
+    print("\t")
+    salir = input("\t¿Quieres salir? Presiona la letra 'S' para salir, o cualquier otra tecla para generar otra contraseña: ")
+    if salir.upper() == 'S':
+        break
